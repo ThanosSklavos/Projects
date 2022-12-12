10dc649d670af62540f99e7b1b352c7ec0df053d
Thanos Sklavos <thanos.sklavos@outlook.com>
2022-12-12 21:35:07 +0200

    Projects v0,1

---
+++ UpperLowerCaseApp.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.projects;
+
+/**
+ * "Hello" -> HeLlo
+ */
+public class UpperLowerCaseApp {
+
+    public static void main(String[] args) {
+        String sentence = "Thanos";
+        StringBuilder sb = new StringBuilder();
+        String s;
+        String substring;
+
+        for (int i =0; i < sentence.length(); i++) {
+            if (Character.isUpperCase(sentence.charAt(i))
+                    || Character.isLowerCase(sentence.charAt(i))) {
+                substring = sentence.substring(i, i + 1);
+                s = ((i % 2) == 0) ? substring.toUpperCase() : substring.toLowerCase();
+                sb.append(s);
+            }
+        }
+
+        for (int i = 0; i < sb.length(); i++) {
+            System.out.print(sb.charAt(i));
+        }
+
+    }
+}
